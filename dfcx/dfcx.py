cae4ead309b293db5a85737dfbd64a920e10e43a
Patrick Marlow <pmarlow@google.com>
2021-02-10 21:14:20 +0000

    Added validate and get_validation_result functions to dfcx

--- dfcx/dfcx.py
+++ dfcx/dfcx.py
@@ -1,9 +1,12 @@
 import json
 import logging
 import os
+import requests
+import subprocess
 import google.cloud.dialogflowcx_v3beta1.services as services
 import google.cloud.dialogflowcx_v3beta1.types as types
 from google.protobuf import field_mask_pb2
+from typing import Dict, List
 
 # logging config
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s',
@@ -100,6 +103,96 @@ class DialogflowCX:
         response = client.create_agent(parent=parent, agent=agent)
 
         return response
+
+    def validate(self, agent_id:str) -> Dict:
+        """Initiates the Validation of the CX Agent or Flow.
+
+        This function will start the Validation feature for the given Agent 
+        and then return the results as a Dict.
+
+        Args:
+          agent_id: CX Agent ID string in the following format
+            projects/<PROJECT ID>/locations/<LOCATION ID>/agents/<AGENT ID>
+
+        Returns:
+          results: Dictionary of Validation results for the entire Agent
+            or for the specified Flow.
+        """
+        location = agent_id.split('/')[3]
+        if location != 'global':
+            base_url = 'https://{}-dialogflow.googleapis.com/v3beta1'.format(location)
+        else:
+            base_url = 'https://dialogflow.googleapis.com/v3beta1'
+
+        url = '{0}/{1}/validationResult'.format(base_url, agent_id)
+        
+        token = subprocess.run(['gcloud', 'auth', 'application-default', 'print-access-token'], 
+                              stdout=subprocess.PIPE,
+                              text=True).stdout
+
+        token = token.strip('\n') # remove newline appended as part of stdout
+        headers = {"Authorization": "Bearer {}".format(token)}
+
+        # Make REST call
+        results = requests.get(url, headers=headers)
+        results.raise_for_status()
+
+        return results.json()
+
+
+    def get_validation_result(self, agent_id:str, flow_id:str=None) -> Dict:
+        """Extract Validation Results from CX Validation feature.
+
+        This function will get the LATEST validation result run for the given
+        CX Agent or CX Flow. If there has been no validation run on the Agent
+        or Flow, no result will be returned. Use `dfcx.validate` function to
+        run Validation on an Agent/Flow.
+
+        Passing in the Agent ID will provide ALL validation results for
+        ALL flows.
+        Passing in the Flow ID will provide validation results for only
+        that Flow ID.
+        
+        Args:
+          agent_id: CX Agent ID string in the following format
+            projects/<PROJECT ID>/locations/<LOCATION ID>/agents/<AGENT ID>
+          flow_id: (Optional) CX Flow ID string in the following format
+            projects/<PROJECT ID>/locations/<LOCATION ID>/agents/<AGENT ID>/flows/<FLOW ID>
+
+        Returns:
+          results: Dictionary of Validation results for the entire Agent
+            or for the specified Flow.
+        """
+
+        if flow_id:
+            location = flow_id.split('/')[3]
+            if location != 'global':
+                base_url = 'https://{}-dialogflow.googleapis.com/v3beta1'.format(location)
+            else:
+                base_url = 'https://dialogflow.googleapis.com/v3beta1'
+
+            url = '{0}/{1}/validationResult'.format(base_url, flow_id)
+        else:
+            location = agent_id.split('/')[3]
+            if location != 'global':
+                base_url = 'https://{}-dialogflow.googleapis.com/v3beta1'.format(location)
+            else:
+                base_url = 'https://dialogflow.googleapis.com/v3beta1'
+
+            url = '{0}/{1}/validationResult'.format(base_url, agent_id)
+        
+        token = subprocess.run(['gcloud', 'auth', 'application-default', 'print-access-token'], 
+                              stdout=subprocess.PIPE,
+                              text=True).stdout
+
+        token = token.strip('\n') # remove newline appended as part of stdout
+        headers = {"Authorization": "Bearer {}".format(token)}
+
+        # Make REST call
+        results = requests.get(url, headers=headers)
+        results.raise_for_status()
+
+        return results.json()
 
 
 ### INTENTS FX    
