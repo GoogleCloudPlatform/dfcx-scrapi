8ee5ca3b0dcb64effcbdbba5663e3faddd655f0c
drescher3 <jupyter@packer-60093374-3b65-1e52-767e-a938621f2ca3.c.deeplearning-platform.internal>
2021-02-25 21:43:26 +0000

    nlu settings function added

--- dfcx/dfcx.py
+++ dfcx/dfcx.py
@@ -512,6 +512,25 @@ class DialogflowCX:
         response = client.update_flow(flow=flow, update_mask=mask)
 
         return response
+    
+    def nlu_settings(self, model_type, classification_threshold, model_training_mode):
+        '''
+        Generates nlu settings objects. 
+        args:
+        flow_id: flow_id to provide the specified settings for
+        model_type:  [0:unspecified, 1:MODEL_TYPE_STANDARD, 2:Custom, 3:Advanced]
+        classification_threshold: treshold for the flow
+        model_training_mode: [0:unspecified, 1:automatic, 2:'manual]
+
+        returns:
+        nlu settings objects which can be passed to the nlu_settings kwarg of the update_flow fxn
+        '''
+
+        nlu_settings = types.flow.NluSettings()
+        nlu_settings.model_type = model_type
+        nlu_settings.classification_threshold = classification_threshold
+        nlu_settings.model_training_mode = model_training_mode
+        return nlu_settings
 
     def export_flow(self,
                     flow_id: str,
