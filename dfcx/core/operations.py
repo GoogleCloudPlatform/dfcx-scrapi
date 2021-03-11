e20a7ccd5bc96896b45b9f28a66c944e8a86bc42
Patrick Marlow <pmarlow@google.com>
2021-03-11 16:48:15 +0000

    new core structure; Operations file and class

---
+++ dfcx/core/operations.py
@@ -0,0 +1,77 @@
+import logging
+import requests
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+
+from typing import Dict, List
+# from dfcx.dfcx import DialogflowCX
+
+# logging config
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+SCOPES = ['https://www.googleapis.com/auth/cloud-platform',
+'https://www.googleapis.com/auth/dialogflow']
+
+class Operations:
+    def __init__(self, creds):
+        self.creds = service_account.Credentials.from_service_account_file(creds,scopes=SCOPES)
+        self.creds.refresh(Request()) # used for REST API calls
+        self.token = self.creds.token # used for REST API calls
+
+    @staticmethod
+    def _set_region(item_id):
+        """different regions have different API endpoints
+
+        Args:
+            item_id: agent/flow/page - any type of long path id like 
+                `projects/<GCP PROJECT ID>/locations/<LOCATION ID>
+
+        Returns:
+            client_options: use when instantiating other library client objects
+        """
+        try:
+            location = item_id.split('/')[3]
+        except IndexError as err:
+            logging.error('IndexError - path too short? %s', item_id)
+            raise err
+
+        if location != 'global':
+            api_endpoint = '{}-dialogflow.googleapis.com:443'.format(location)
+            client_options = {'api_endpoint': api_endpoint}
+            return client_options
+
+        else:
+            return None # explicit None return when not required
+
+# OPERATIONS FX
+
+    def get_lro(self, lro: str) -> Dict[str,str]:
+        """Used to retrieve the status of LROs for Dialogflow CX.
+
+        Args:
+          lro: The Long Running Operation(LRO) ID in the following format
+              'projects/<project-name>/locations/<locat>/operations/<operation-uuid>'
+
+        Returns:
+          response: Response status and payload from LRO
+              
+        """
+
+        location = lro.split('/')[3]
+        if location != 'global':
+            base_url = 'https://{}-dialogflow.googleapis.com/v3beta1'.format(
+                location)
+        else:
+            base_url = 'https://dialogflow.googleapis.com/v3beta1'
+
+        url = '{0}/{1}'.format(base_url, lro)
+        headers = {"Authorization": "Bearer {}".format(self.token)}
+
+        # Make REST call
+        results = requests.get(url, headers=headers)
+        results.raise_for_status()
+
+        return results.json()
