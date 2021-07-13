4cfc1e76341b851736d3882b5337a1ab553a7057
Patrick Marlow <pmarlow@google.com>
2021-07-13 15:17:24 -0500

    lint updates

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,28 +1,31 @@
+"""Test Class for Agent Functions in SAPI lib."""
 # Copyright 2021 Google LLC. This software is provided as-is, without warranty
 # or representation for any use or purpose. Your use of it is subject to your
 # agreement with Google.
 
-import pytest
 import time
+from datetime import datetime
+# from typing import List, Dict
+
+import pytest
 import google.cloud.dialogflowcx_v3beta1.types as types
 from dfcx_sapi.core.agents import Agents
 from dfcx_sapi.core.operations import Operations
-from datetime import datetime
-from typing import List, Dict
-
-# TODO Create SA for Gitlab CICD and configure as ENV VAR
 
 DEV = True  # Set flag to disable some tests while in development
 
 today_time = datetime.now().strftime("%d%m%Y_%H%M%S")
 AGENT_NAME = "DFCX SAPI - TEMP TEST AGENT {}".format(today_time)
 CREDS_PATH = None
+PROJECT_ID = None
 
 
 @pytest.fixture(name="cx_vars", scope="class")
 def cx_vars_fixture():
+    """Fixture method to setup shared vars across test runs."""
     class CxVars:
-        def __init__(self, CREDS_PATH):
+        """Class to hold shared vars across test runs."""
+        def __init__(self, CREDS_PATH): # pylint: disable=W0621
             self.agents = Agents(CREDS_PATH)
             self.ops = Operations(CREDS_PATH)
             self.gcs_bucket_uri = "gs://dfcx_scrapi/api_test_agent.json"
@@ -31,16 +34,19 @@ def cx_vars_fixture():
 
 
 class TestAgents:
+    """Main class to test all SAPI agent methods."""
     def test_create_agents(self, cx_vars):
+        """Tests the SAPI method create_agent from core/agents.py"""
         if DEV:
             cx_vars.temp_agent = cx_vars.agents.create_agent(
-                "nj-pods-dev", AGENT_NAME
+                PROJECT_ID, AGENT_NAME
             )
 
             assert isinstance(cx_vars.temp_agent, types.Agent)
             assert cx_vars.temp_agent.display_name == AGENT_NAME
 
     def test_restore_agent(self, cx_vars):
+        """Tests the SAPI method restore_agent from core/agents.py"""
         lro = cx_vars.agents.restore_agent(
             cx_vars.temp_agent.name, gcs_bucket_uri=cx_vars.gcs_bucket_uri
         )
@@ -52,12 +58,14 @@ class TestAgents:
         assert res["done"]
 
     def test_get_agent(self, cx_vars):
+        """Tests the SAPI method get_agent from core/agents.py"""
         agent = cx_vars.agents.get_agent(cx_vars.temp_agent.name)
 
         assert isinstance(agent, types.Agent)
         assert cx_vars.temp_agent.display_name == AGENT_NAME
 
     def test_export_agent(self, cx_vars):
+        """Tests the SAPI method export_agent from core/agents.py"""
         lro = cx_vars.agents.export_agent(
             cx_vars.temp_agent.name,
             "{}/testing_export.json".format(cx_vars.gcs_bucket_uri),
@@ -68,6 +76,7 @@ class TestAgents:
         assert cx_vars.ops.get_lro(lro)["done"]
 
     def test_update_agent(self, cx_vars):
+        """Tests the SAPI method update_agent from core/agents.py"""
         temp_name = AGENT_NAME + "_UPDATED"
         res = cx_vars.agents.update_agent(
             cx_vars.temp_agent.name, display_name=temp_name
@@ -76,6 +85,7 @@ class TestAgents:
         assert res.display_name == temp_name
 
     def test_delete_agent(self, cx_vars):
+        """Tests the SAPI method delete_agent from core/agents.py"""
         res = cx_vars.agents.delete_agent(cx_vars.temp_agent.name)
 
         print(res)
@@ -83,12 +93,8 @@ class TestAgents:
         assert cx_vars.temp_agent.name in res
 
     def test_list_agents(self, cx_vars):
-        location_id = "projects/nj-pods-dev/locations/global"
+        """Tests the SAPI method list_agents from core/agents.py"""
+        location_id = "projects/%s/locations/global", PROJECT_ID
         agents = cx_vars.agents.list_agents(location_id)
 
         assert isinstance(agents[0], types.Agent)
-
-    # TODO
-    # def test_validate
-
-    # def test_get_validation_result
