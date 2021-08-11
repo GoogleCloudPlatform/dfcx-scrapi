5cd97c9614deb3d05aa1f930fbebbadb8600f602
Patrick Marlow <pmarlow@google.com>
2021-08-11 16:33:43 +0000

    Stats util 196215704

--- src/dfcx_scrapi/tools/stats_util.py
+++ src/dfcx_scrapi/tools/stats_util.py
@@ -20,7 +20,8 @@ from dfcx_scrapi.core.scrapi_base import ScrapiBase
 from dfcx_scrapi.core.intents import Intents
 from dfcx_scrapi.core.flows import Flows
 from dfcx_scrapi.core.pages import Pages
-
+from dfcx_scrapi.core.entity_types import EntityTypes
+from dfcx_scrapi.core.transition_route_groups import TransitionRouteGroups
 
 class StatsUtil(ScrapiBase):
     """A util class to provide common stats for a CX Agent."""
@@ -40,20 +41,37 @@ class StatsUtil(ScrapiBase):
             scope=scope,
         )
 
-        self.intents_tracker = Intents(creds=self.creds)
-        self.flows_tracker = Flows(creds=self.creds)
-        self.pages_tracker = Pages(creds=self.creds)
-
         if agent_id:
             self.agent_id = agent_id
 
-    def list_all_pages(self):
+        self.intents_tracker = Intents(
+            creds=self.creds, agent_id=self.agent_id)
+        self.flows_tracker = Flows(
+            creds=self.creds, agent_id=self.agent_id)
+        self.pages_tracker = Pages(creds=self.creds)
+        self.entity_tracker = EntityTypes(
+            creds=self.creds, agent_id=self.agent_id)
+        self.rg_tracker = TransitionRouteGroups(
+            creds=self.creds, agent_id=self.agent_id
+        )
+
+    def _get_flows_map(self, agent_id: str = None):
+        return self.flows_tracker.get_flows_map(agent_id)
+
+    def _list_all_pages(self, flows):
         """Get a List of all pages from every flow."""
-        flows = self.flows_tracker.list_flows()
         pages = []
         for flow in flows:
-            pages += self.pages_tracker.list_pages(flow.name)
+            pages += self.pages_tracker.list_pages(flow)
         return pages
+
+    def _list_all_rgs(self, flows):
+        """Get a list of all route groups from every flow."""
+        rgs = []
+        for flow in flows:
+            rgs += self.rg_tracker.list_transition_route_groups(flow)
+
+        return rgs
 
     def stats(self, agent_id: str = None):
         """snapshot of an agents state"""
@@ -61,12 +79,19 @@ class StatsUtil(ScrapiBase):
         if not agent_id:
             agent_id = self.agent_id
 
+        flows_map = self._get_flows_map(agent_id)
+
         all_intents = self.intents_tracker.bulk_intent_to_df(agent_id=agent_id)
-        flows_map = self.flows_tracker.get_flows_map(agent_id)
+        all_entity_types = self.entity_tracker.list_entity_types()
+        all_pages = self._list_all_pages(flows_map)
+        all_rgs = self._list_all_rgs(flows_map)
+
         info = {
             "Total # of Flows": len(flows_map.keys()),
+            "Total # of Pages": len(all_pages),
             "Total # of Intents": all_intents.intent.nunique(),
             "Total # of Training Phrases": all_intents.shape[0],
-            "Total # of Pages": len(self.list_all_pages()),
+            "Total # of Entity Types": len(all_entity_types),
+            "Total # of Route Groups": len(all_rgs)
         }
         return info
