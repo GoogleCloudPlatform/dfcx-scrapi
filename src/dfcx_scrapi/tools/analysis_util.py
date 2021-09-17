50570b636f0fd0f8f26d80877f4d847ba1693637
Omer Zakay <omer.zakay@gmail.com>
2021-09-17 19:37:17 +0000

    added tool for performing levenshtein on two intents

---
+++ src/dfcx_scrapi/tools/analysis_util.py
@@ -0,0 +1,162 @@
+"""Util class for performing analysis on DFCX objects and data."""
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import pandas
+import numpy as np
+from dfcx_scrapi.core.intents import Intents
+import google.cloud.dialogflowcx_v3beta1.types as types
+
+# logging config
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+class AnalysisUtils():
+    """Utils class for quickly building DFCX webhook payloads."""
+
+    @staticmethod
+    def levenshtein_ratio(s, t):
+        """ levenshtein_ratio_and_distance:
+            Calculates levenshtein distance between two strings.
+        """
+        rows = len(s)+1
+        cols = len(t)+1
+        distance = np.zeros((rows,cols),dtype = int)
+
+        for i in range(1, rows):
+            for k in range(1,cols):
+                distance[i][0] = i
+                distance[0][k] = k
+
+        for col in range(1, cols):
+            for row in range(1, rows):
+                if s[row-1] == t[col-1]:
+                    cost = 0
+                else:
+                    cost = 2
+
+                distance[row][col] =  min(
+                    distance[row-1][col] + 1,
+                    distance[row][col-1] + 1,
+                    distance[row-1][col-1] + cost
+                )
+
+        # Computation of the Levenshtein Distance Ratio
+        ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
+        return ratio
+
+
+    @staticmethod
+    def calc_tp_distances(
+        intent_key,
+        intent_comparator,
+        threshold=0.75,
+        silent=False
+    ):
+        '''
+        compares the training phrases between two intents to find the
+        levenshtein distance between all training phrases. Structure of the
+        returned object depends on which intent is the key, and which is the
+        comparator.
+
+        Args:
+          intent_key: intent protobuf or dataframe object. In the returned
+          object, the utterances of this intent are paired with a list of
+          utterances from the comparator.
+
+          intent_comparator: intent protobuf or dataframe object. In the
+          returned object, the utterances of this intent will be included
+          in a list associated with every phrase whose similarity ratio
+          is above the defined threshold.
+
+          threshold: float describing the levenshtein distance above which
+          a pair of phrases should be associated. Default: .75
+
+        Returns:
+          tp_distances: Object listing each training phrase in intent_key.
+          Associated with each intent_key will be a list of pairings,
+          where each pair represents an utterance from intent_comparator
+          with a similarity ratio above the defined threshold.
+        '''
+
+        if isinstance(intent_key, types.Intent):
+            list_keys = Intents.intent_proto_to_dataframe(
+                intent_key
+            ).tp
+        elif isinstance(intent_key, pandas.core.frame.DataFrame):
+            list_keys = intent_key.tp
+        else:
+            logging.warning(
+                '''parameter list_keys must be of type
+                dialogflowcx_v3beta1.types.intent.Intent
+                or pandas.core.frame.DataFrame.
+                ''')
+            return None
+
+        if isinstance(intent_comparator, types.Intent):
+            list_comparators = Intents.intent_proto_to_dataframe(
+                intent_comparator
+            ).tp
+        elif isinstance(intent_comparator, pandas.core.frame.DataFrame):
+            list_comparators = intent_comparator.tp
+        else:
+            logging.warning(
+                '''parameter intent_comparator must be of type
+                dialogflowcx_v3beta1.types.intent.Intent
+                or pandas.core.frame.DataFrame.
+                ''')
+            return None
+
+        it = 0
+        completed = 0.0
+        tp_distances = {}
+
+        for line1 in list_keys:
+
+            phrase_similarity_list = {}
+
+            if not silent:
+                completed = float(it/len(list_keys))*100.0
+                print(
+                    ' {}% complete. \r'.format(round(completed,1)),
+                    end='',
+                    flush=True
+                     )
+
+            for line2 in list_comparators:
+                distance = AnalysisUtils.levenshtein_ratio(
+                    line1,
+                    line2
+                )
+
+                if distance>threshold:
+                    phrase_similarity_list[line2] = round(distance,3)
+
+            it += 1
+
+            #float greatest similarities to top
+            phrase_similarity_list = dict(sorted(
+                phrase_similarity_list.items(),
+                key=lambda item: item[1],
+                reverse=True,))
+
+            tp_distances[line1] = phrase_similarity_list
+        if not silent:
+            print('100% -- Done.')
+
+        return tp_distances
