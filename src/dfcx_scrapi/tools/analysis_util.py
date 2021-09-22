5f71291caba49b691a603d510070f1f237126d14
Omer Zakay <omer.zakay@gmail.com>
2021-09-22 19:21:38 +0000

    minor fixes based on feedback, example included

--- src/dfcx_scrapi/tools/analysis_util.py
+++ src/dfcx_scrapi/tools/analysis_util.py
@@ -17,20 +17,21 @@
 import logging
 import pandas
 import numpy as np
+from typing import Union
 from dfcx_scrapi.core.intents import Intents
 import google.cloud.dialogflowcx_v3beta1.types as types
 
 # logging config
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S")
 
-class AnalysisUtils():
-    """Utils class for quickly building DFCX webhook payloads."""
+class AnalysisUtil():
+    """Utils class for performing analysis on DFCX data."""
 
     @staticmethod
-    def levenshtein_ratio(s, t):
+    def __levenshtein_ratio(s: str, t: str) -> float:
         """ levenshtein_ratio_and_distance:
             Calculates levenshtein distance between two strings.
         """
@@ -63,12 +64,12 @@ class AnalysisUtils():
 
     @staticmethod
     def calc_tp_distances(
-        intent_key,
-        intent_comparator,
-        threshold=0.75,
-        silent=False
+        intent_key: Union[types.Intent, pandas.core.frame.DataFrame],
+        intent_comparator: Union[types.Intent, pandas.core.frame.DataFrame],
+        threshold: float = 0.75,
+        silent: bool = False
     ):
-        '''
+        """
         compares the training phrases between two intents to find the
         levenshtein distance between all training phrases. Structure of the
         returned object depends on which intent is the key, and which is the
@@ -87,12 +88,16 @@ class AnalysisUtils():
           threshold: float describing the levenshtein distance above which
           a pair of phrases should be associated. Default: .75
 
+          silent: When set to True, the program will execute without 
+          creating Info logs or updating the progress on the console.
+          Default=False
+
         Returns:
           tp_distances: Object listing each training phrase in intent_key.
           Associated with each intent_key will be a list of pairings,
           where each pair represents an utterance from intent_comparator
           with a similarity ratio above the defined threshold.
-        '''
+        """
 
         if isinstance(intent_key, types.Intent):
             list_keys = Intents.intent_proto_to_dataframe(
@@ -102,10 +107,10 @@ class AnalysisUtils():
             list_keys = intent_key.tp
         else:
             logging.warning(
-                '''parameter list_keys must be of type
+                """parameter list_keys must be of type
                 dialogflowcx_v3beta1.types.intent.Intent
                 or pandas.core.frame.DataFrame.
-                ''')
+                """)
             return None
 
         if isinstance(intent_comparator, types.Intent):
@@ -116,36 +121,48 @@ class AnalysisUtils():
             list_comparators = intent_comparator.tp
         else:
             logging.warning(
-                '''parameter intent_comparator must be of type
+                """parameter intent_comparator must be of type
                 dialogflowcx_v3beta1.types.intent.Intent
                 or pandas.core.frame.DataFrame.
-                ''')
+                """)
             return None
 
         it = 0
         completed = 0.0
         tp_distances = {}
+        num_keys_overlapped = 0
+        num_comparators_overlapped = 0
 
         for line1 in list_keys:
 
             phrase_similarity_list = {}
+            found_key_similarity = False
 
             if not silent:
                 completed = float(it/len(list_keys))*100.0
+                #print instead of logging.info is intentional.
+                #Needed to avoid spamming new lines with every percentile.
                 print(
-                    ' {}% complete. \r'.format(round(completed,1)),
-                    end='',
+                    " {}% complete. \r".format(round(completed,1)),
+                    end="",
                     flush=True
                      )
 
             for line2 in list_comparators:
-                distance = AnalysisUtils.levenshtein_ratio(
+
+                distance = AnalysisUtil.__levenshtein_ratio(
                     line1,
                     line2
                 )
 
                 if distance>threshold:
                     phrase_similarity_list[line2] = round(distance,3)
+                    num_comparators_overlapped += 1
+
+                    if found_key_similarity is False:
+                        num_keys_overlapped += 1
+                        found_key_similarity = True
+
 
             it += 1
 
@@ -156,7 +173,33 @@ class AnalysisUtils():
                 reverse=True,))
 
             tp_distances[line1] = phrase_similarity_list
-        if not silent:
-            print('100% -- Done.')
 
-        return tp_distances
+        #calculate statistics
+        total_keys = len(list_keys)
+        key_percent_overlap = 100*num_keys_overlapped/total_keys
+        key_percent_overlap = round(key_percent_overlap, 3)
+
+        total_comps = len(list_comparators)*total_keys
+        comp_percent_overlap = 100*num_comparators_overlapped/total_comps
+        comp_percent_overlap = round(comp_percent_overlap, 3)
+
+        stats = {"keys":{
+                    "total:":total_keys,
+                    "num_overlap":num_keys_overlapped,
+                    "percent_overlap":"{}%".format(key_percent_overlap)
+                    },
+                 "comparators":{
+                    "total:":total_comps,
+                    "num_overlap":num_comparators_overlapped,
+                    "percent_overlap":"{}%".format(comp_percent_overlap)
+                    }
+                }
+
+        if not silent:
+            #print instead of logging.info intentional.
+            print("100% -- Done.      ", flush=True)
+            print("")
+            logging.info("Statistics:\n%s", stats)
+
+        results = {"stats":stats,"distances":tp_distances}
+        return results
