ffa3ac99fdcca031fdddbb81b14d6861af013865
Patrick Marlow <kmaphoenix@gmail.com>
2022-02-08 00:32:42 -0600

    class refactor and linting

--- src/dfcx_scrapi/core/environments.py
+++ src/dfcx_scrapi/core/environments.py
@@ -14,12 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import logging
-from typing import Dict
-import google.cloud.dialogflowcx_v3beta1.services as services
-import google.cloud.dialogflowcx_v3beta1.types as types
-import google.protobuf.field_mask_pb2 as fm_pb
+from google.cloud.dialogflowcx_v3beta1 import services
+from google.cloud.dialogflowcx_v3beta1 import types
+from google.protobuf import field_mask_pb2
 from dfcx_scrapi.core.scrapi_base import ScrapiBase
+from dfcx_scrapi.core.flows import Flows
+from dfcx_scrapi.core.versions import Versions
 
 # logging config
 logging.basicConfig(
@@ -35,7 +38,7 @@ class Environments(ScrapiBase):
     def __init__(
         self,
         creds_path: str = None,
-        creds_dict: Dict = None,
+        creds_dict: dict = None,
         creds=None,
         agent_id: str = None,
     ):
@@ -47,6 +50,52 @@ class Environments(ScrapiBase):
 
         if agent_id:
             self.agent_id = agent_id
+
+        self._versions = Versions(creds=self.creds)
+        self._flows = Flows(creds=self.creds)
+
+    @staticmethod
+    def _get_flow_version_id(
+        input_tuple: tuple[str,str],
+        flow_versions_list: list[types.Version]):
+        """Parse Version ID based on Tuple inputs."""
+        for version in flow_versions_list:
+            if version.name.split("/")[-1] == str(input_tuple[1]):
+                return version.name
+
+        return print("Flow Name and Version combination not found in Agent.")
+
+    def _get_filtered_flow_map(self, agent_id, flow_tuples):
+        """Get Flows and Filter based on User Input"""
+        flows_map = self._flows.get_flows_map(agent_id, reverse=True)
+        input_flows = [flow[0] for flow in flow_tuples]
+        filtered_map = {}
+        for flow in input_flows:
+            if flow in flows_map.keys():
+                filtered_map[flow] = flows_map[flow]
+
+        return filtered_map
+
+    def _get_filtered_versions(self, filtered_map):
+        """Get filtered versions Dict based on filtered Flows map"""
+        filtered_versions = {}
+        for flow in filtered_map:
+            filtered_versions[flow] = self._versions.list_versions(
+                filtered_map[flow])
+
+        return filtered_versions
+
+    def _get_final_versions(self, flow_tuples, filtered_versions):
+        """Determine associated Flow Version IDs based on input tuple"""
+        final_versions = []
+        for version_tuple in flow_tuples:
+            version_config = types.environment.Environment.VersionConfig()
+            version_config.version = self._get_flow_version_id(
+                version_tuple, filtered_versions[version_tuple[0]])
+
+            final_versions.append(version_config)
+
+        return final_versions
 
     def list_environments(self, agent_id:str=None):
         """List all Versions for a given Flow"""
@@ -73,62 +122,37 @@ class Environments(ScrapiBase):
 
     def get_environment(
         self,
-        environment_id:str=None,
-        display_name:str=None,
-        agent_id:str=None
-    ) -> types.environment.Environment:
-        """Get Environment object for specified environment.
-        Requires either environment's ID or display name.
-        If both are provided, display_name is considered first.
+        environment_id:str) -> types.environment.Environment:
+        """Get Environment object for specified environment ID.
 
         Args:
           environment_id: Unique environment ID of the target to get. Format:
             projects/<Project ID>/locations/<Location ID>/agents/
             <Agent ID>/environments/<Environment ID>
 
-          display_name: target environment's human-legible display name.
-
-          agent_id: The targeted agent for the operation. Defaults to self.agent_id. format:
-            projects/<Project ID>/locations/<Location ID>/agents/<Agent ID>
-
         Returns:
             Environment object.
         """
+        request = types.environment.GetEnvironmentRequest(
+            name=environment_id)
+        client_options = self._set_region(environment_id)
+        client = services.environments.EnvironmentsClient(
+            client_options=client_options, credentials=self.creds
+        )
 
-        if not display_name and not environment_id:
-            logging.warning("get_environment() requires environment_id or display_name.")
-            return None
-
-        if not agent_id:
-            agent_id = self.agent_id
-
-        if display_name:
-            response = self.get_environment_by_display_name(display_name)
-
-        else:
-            request = types.environment.GetEnvironmentRequest(name=environment_id)
-            client_options = self._set_region(agent_id)
-            client = services.environments.EnvironmentsClient(
-                client_options=client_options, credentials=self.creds
-            )
-
-            response = client.get_environment(request)
+        response = client.get_environment(request)
 
         return response
 
     def get_environment_by_display_name(
         self,
         display_name:str,
-        agent_id:str=None
-    ) -> types.environment.Environment:
+        agent_id:str) -> types.environment.Environment:
         """Get Environment object for specific environment by its display name.
-        This technically duplicates get_environment(), but one uses the other,
-        and I see no reason to keep it as private.
 
         Args:
           display_name: Human readable display name of the target to get.
-
-          agent_id: The agent for the operation. Defaults to self.agent_id. format:
+          agent_id: The agent for the operation. format:
             projects/<Project ID>/locations/<Location ID>/agents/<Agent ID>
 
         Returns:
@@ -145,19 +169,16 @@ class Environments(ScrapiBase):
 
         return result
 
-
     def create_environment(
         self,
         environment:types.environment.Environment,
         agent_id:str=None):
         """Create a new environment for a specified agent.
         Args:
-          environment: Required. The environment to create.
+          environment: The environment to create.
             type google.cloud.dialogflowcx_v3beta1.types.Environment
-
-          agent_id: The targeted agent for the operation. Defaults to self.agent_id. format:
+          agent_id: The targeted agent for the operation. format:
             projects/<Project ID>/locations/<Location ID>/agents/<Agent ID>
-
         Returns:
           An object representing a long-running operation. (LRO)
         """
@@ -165,9 +186,60 @@ class Environments(ScrapiBase):
         if not agent_id:
             agent_id = self.agent_id
 
+        request = types.environment.CreateEnvironmentRequest()
+        request.environment = environment
+        request.parent = agent_id
+
+        client_options = self._set_region(agent_id)
+        client = services.environments.EnvironmentsClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.create_environment(request)
+
+        return response
+
+
+    def create_environment_by_display_name(
+        self,
+        display_name:str,
+        version_configs:list[tuple(str,str)],
+        description:str=None,
+        agent_id:str=None):
+        """Create a new environment for a specified agent.
+        Args:
+          display_name: The display name of the Environment to create
+          version_configs: A List of Tuples, containing the Flow Display
+            Names and Version IDs to include in the Environment creation.
+            Ex:
+              [('Default Start Flow', 2), ('Confidence Demo', 3), ('FlowC', 1)]
+
+            Internally, the create_environment method will perform a lookup and
+            pull the appropriate Flow Version IDs to assign to the Environment
+            being created.
+          agent_id: The targeted agent for the operation. format:
+            projects/<Project ID>/locations/<Location ID>/agents/<Agent ID>
+
+        Returns:
+          response: An object representing a long-running operation (LRO).
+            View the result by inspecting response.result()
+        """
+
+        if not agent_id:
+            agent_id = self.agent_id
+
+        filtered_map = self._get_filtered_flow_map(agent_id, version_configs)
+        all_versions = self._get_filtered_versions(filtered_map)
+        final_versions = self._get_final_versions(version_configs, all_versions)
+
+        # Build the Environment Object
+        env = types.Environment()
+        env.display_name = display_name
+        env.version_configs = final_versions
+        env.description = description
 
         request = types.environment.CreateEnvironmentRequest(
-            environment = environment,
+            environment = env,
             parent = agent_id
         )
 
@@ -183,38 +255,42 @@ class Environments(ScrapiBase):
 
     def update_environment(
         self,
-        environment:types.environment.Environment,
-        update_mask:fm_pb.FieldMask,
-        agent_id:str=None):
+        environment_id: str,
+        environment_obj:types.Environment = None,
+        **kwargs):
         """Update an existing environment for a specified agent.
 
         Args:
-          environment: Required. The environment to update.
-            type google.cloud.dialogflowcx_v3beta1.types.Environment
-
-          update_mask: Required. FieldMask object indicating which
-            fields are to be changed.
-
-          agent_id: The targeted agent for the operation. Defaults to self.agent_id. format:
-            projects/<Project ID>/locations/<Location ID>/agents/<Agent ID>
+          environment_id: The specified environment to update.
+          environment_obj: Optional Environment object of types.Environment
+            that can be provided when you are planning to replace the full
+            object vs. just partial updates.
 
         Returns:
           An object representing a long-running operation. (LRO)
         """
 
-        if not agent_id:
-            agent_id = self.agent_id
+        if environment_obj:
+            env = environment_obj
+        else:
+            env = types.Environment()
 
+        env.name = environment_id
 
-        request = types.environment.UpdateEnvironmentRequest(
-            environment = environment,
-            update_mask = update_mask
-        )
+        # set environment attributes from kwargs
+        for key, value in kwargs.items():
+            setattr(env, key, value)
+        paths = kwargs.keys()
+        mask = field_mask_pb2.FieldMask(paths=paths)
 
-        client_options = self._set_region(agent_id)
+        client_options = self._set_region(environment_id)
         client = services.environments.EnvironmentsClient(
-            client_options=client_options, credentials=self.creds
+            credentials=self.creds, client_options=client_options
         )
+
+        request = types.environment.UpdateEnvironmentRequest()
+        request.environment = env
+        request.update_mask = mask
 
         response = client.update_environment(request)
 
@@ -230,13 +306,12 @@ class Environments(ScrapiBase):
             <Agent ID>/environments/<Environment ID>.
         """
 
-        request = types.environment.DeleteEnvironmentRequest(
-            name = environment_id
-        )
+        request = types.environment.DeleteEnvironmentRequest()
+        request.name = environment_id
 
         client_options = self._set_region(environment_id)
         client = services.environments.EnvironmentsClient(
-            client_options=client_options, credentials=self.creds
+            credentials=self.creds, client_options=client_options
         )
 
         client.delete_environment(request)
@@ -247,7 +322,6 @@ class Environments(ScrapiBase):
         environment_id:str,
         flow_version:str):
         """Deploys a flow to the specified environment.
-
          Args:
           environment_id: unique ID associated with target environment. Format:
             projects/<Project ID>/locations/<Location ID>/agents/
@@ -255,26 +329,27 @@ class Environments(ScrapiBase):
           flow_version: Required. The flow version to deploy. Format:
             projects/<Project ID>/locations/<Location ID>/agents/
             <Agent ID>/ flows/<Flow ID>/versions/<Version ID>
-
         Returns:
           An object representing a long-running operation. (LRO)
         """
 
-        request = types.environment.DeployFlowRequest(
-            environment = environment_id,
-            flow_version = flow_version
-        )
+        request = types.environment.DeployFlowRequest()
+        request.environment = environment_id
+        request.flow_version = flow_version
 
         client_options = self._set_region(environment_id)
         client = services.environments.EnvironmentsClient(
-            client_options=client_options, credentials=self.creds
+            credentials=self.creds, client_options=client_options
         )
 
         response = client.deploy_flow(request)
+
         return response
 
 
-    def lookup_environment_history(self, environment_id:str):
+    def lookup_environment_history(
+        self,
+        environment_id:str) -> list[types.Environment]:
         """Looks up the history of the specified environment.
 
         Args:
@@ -283,9 +358,7 @@ class Environments(ScrapiBase):
             <Agent ID>/environments/<Environment ID>.
 
         Returns:
-          environments.pagers.LookupEnvironmentHistoryPager:
-            Iterating over this object will yield results and
-            resolve additional pages automatically.
+          history: List of Environment objects with historical timestamps
         """
         request = types.environment.LookupEnvironmentHistoryRequest(
             name = environment_id
@@ -298,7 +371,12 @@ class Environments(ScrapiBase):
 
         response = client.lookup_environment_history(request)
 
-        return response
+        history = []
+        for page in response.pages:
+            for environment in page.environments:
+                history.append(environment)
+
+        return history
 
     def list_continuous_test_results(self, environment_id:str):
         """Fetches a list of continuous test results for a given environment.
@@ -309,9 +387,7 @@ class Environments(ScrapiBase):
             <Agent ID>/environments/<Environment ID>.
 
         Returns:
-          environments.pagers.ListContinuousTestResultsPager:
-            Iterating over this object will yield results and
-            resolve additional pages automatically.
+          test_results: List of types.ContinuousTestResult
         """
 
         request = types.environment.ListContinuousTestResultsRequest(
@@ -325,4 +401,9 @@ class Environments(ScrapiBase):
 
         response = client.list_continuous_test_results(request)
 
-        return response
+        test_results = []
+        for page in response.pages:
+            for test in page.continuous_test_results:
+                test_results.append(test)
+
+        return test_results
