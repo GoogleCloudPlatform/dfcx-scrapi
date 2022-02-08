4ebae8c631b6d9e93e06f57da4982a98b5281037
Patrick Marlow <kmaphoenix@gmail.com>
2022-02-08 12:39:58 -0600

    linting and final updates

--- src/dfcx_scrapi/core/security_settings.py
+++ src/dfcx_scrapi/core/security_settings.py
@@ -32,9 +32,9 @@ logging.basicConfig(
 )
 
 class SecuritySettings(ScrapiBase):
-  """Core Class for CCAI Security Settings."""
+    """Core Class for CCAI Security Settings."""
 
-  def __init__(
+    def __init__(
         self,
         creds_path: str = None,
         creds_dict: dict = None,
@@ -51,139 +51,165 @@ class SecuritySettings(ScrapiBase):
 
         if agent_id:
             self.agent_id = agent_id
-  
-  def list_security_settings(self, location_id: str):
-      """List Security Settings for a given Project and Region.
 
-      Args:
-          location_id: CX Agent ID string in the following format
-          projects/<PROJECT ID>/locations/<LOCATION ID>/
+        self.ss_service = services.security_settings_service
+        self.ss_types = types.security_settings
 
-      Returns:
-          results: Dict of results
-      """
+    def list_security_settings(self, location_id: str):
+        """List Security Settings for a given Project and Region.
 
-      request = types.security_settings.ListSecuritySettingsRequest()
+        Args:
+            location_id: CX Agent ID string in the following format
+            projects/<PROJECT ID>/locations/<LOCATION ID>/
 
-      request.parent = location_id
-      client_options = self._set_region(location_id)
-      client = services.security_settings_service.SecuritySettingsServiceClient(
-          credentials=self.creds, client_options=client_options
-      )
+        Returns:
+            results: Dict of results
+        """
 
-      response = client.list_security_settings(request)
+        request = self.ss_types.ListSecuritySettingsRequest()
 
-      security_settings = []
-      for page in response.pages:
-        for ss in page.security_settings:
-          security_settings.append(ss)
-
-      return security_settings
-
-
-  def get_security_settings(self, security_setting_id: str):
-      """Get specified CCAI Security Setting.
-
-      Args:
-          security_setting_id: Security Setting ID string in the following
-            format: projects/<PROJECT ID>/locations/<LOCATION ID>/
-            securitySettings/<SECURITY SETTINGS ID>
-
-      Returns:
-          response: a single Security Settings object of types.SecuritySettings
-      """
-
-      request = types.security_settings.GetSecuritySettingsRequest()
-
-      request.name = security_setting_id
-      client_options = self._set_region(security_setting_id)
-      client = services.security_settings_service.SecuritySettingsServiceClient(
-          credentials=self.creds, client_options=client_options
-      )
-
-      response = client.get_security_settings(request)
-
-      return response
-
-  def create_security_settings(
-      self,
-      project_id: str,
-      obj: types.SecuritySettings = None,
-      security_settings_dict: dict[str,str] = None):
-      """Create CCAI Security Settings profile in the specified project.
-
-      One of `obj` or `security_settings_dict` should be provided to create a
-      new security settings profile.
-
-      Args:
-        project_id: the GCP project ID string where the Security Settings should
-          be created
-        obj: An object of types.SecuritySettings representing the Security
-          Settings object to be created
-        security_settings_dict: An optional dictionary of key/value pairs that
-          correspond to the fields and values necessary for creating a new
-          Security Settings profile
-
-      Returns:
-          response: a single Security Settings object of types.SecuritySettings
-      """
-
-      if obj and security_settings_dict:
-        raise ValueError("Cannot provide both obj and security_settings_dict")
-      elif obj:
-        security_settings = obj
-        security_settings.name = ""
-      elif security_settings_dict:
-        security_settings = types.security_settings.SecuritySettings.from_json(
-            json.dumps(security_settings_dict)
+        request.parent = location_id
+        client_options = self._set_region(location_id)
+        client = self.ss_service.SecuritySettingsServiceClient(
+            credentials=self.creds, client_options=client_options
         )
-      else:
-        raise ValueError("Must provide either obj or security_settings_dict")
 
-      request = types.security_settings.CreateSecuritySettingsRequest()
+        response = client.list_security_settings(request)
 
-      request.parent = project_id
-      request.security_settings = security_settings
+        security_settings = []
+        for page in response.pages:
+            for ss in page.security_settings:
+                security_settings.append(ss)
 
-      client_options = self._set_region(project_id)
-      client = services.security_settings_service.SecuritySettingsServiceClient(
-          credentials=self.creds, client_options=client_options
-      )
+        return security_settings
 
-      response = client.create_security_settings(request)
 
-      return response
+    def get_security_settings(self, security_setting_id: str):
+        """Get specified CCAI Security Setting.
 
-  def update_security_settings(self, security_setting_id: str, **kwargs):
-      """Update specified CCAI Security Setting.
+        Args:
+            security_setting_id: Security Setting ID string in the following
+                format: projects/<PROJECT ID>/locations/<LOCATION ID>/
+                securitySettings/<SECURITY SETTINGS ID>
 
-      Args:
+        Returns:
+            response: a single Security Settings object of
+              types.SecuritySettings
+        """
+
+        request = self.ss_types.GetSecuritySettingsRequest()
+
+        request.name = security_setting_id
+        client_options = self._set_region(security_setting_id)
+        client = self.ss_service.SecuritySettingsServiceClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.get_security_settings(request)
+
+        return response
+
+    def create_security_settings(
+        self,
+        location_id: str,
+        obj: types.SecuritySettings = None,
+        security_settings_dict: dict[str,str] = None):
+        """Create CCAI Security Settings profile in the specified project.
+
+        One of `obj` or `security_settings_dict` should be provided to create a
+        new security settings profile.
+
+        Args:
+            location_id: CX Agent ID string in the following format:
+              projects/<PROJECT ID>/locations/<LOCATION ID>/
+            obj: An object of types.SecuritySettings representing the Security
+            Settings object to be created
+            security_settings_dict: An optional dictionary of key/value pairs
+            that correspond to the fields and values necessary for creating a
+            new Security Settings profile
+
+        Returns:
+            response: a single Security Settings object of
+              types.SecuritySettings
+        """
+
+        if obj and security_settings_dict:
+            raise ValueError(
+                "Cannot provide both obj and security_settings_dict")
+        elif obj:
+            security_settings = obj
+            security_settings.name = ""
+        elif security_settings_dict:
+            security_settings = self.ss_types.SecuritySettings.from_json(
+                json.dumps(security_settings_dict)
+            )
+        else:
+            raise ValueError(
+                "Must provide either obj or security_settings_dict")
+
+        request = self.ss_types.CreateSecuritySettingsRequest()
+
+        request.parent = location_id
+        request.security_settings = security_settings
+
+        client_options = self._set_region(location_id)
+        client = self.ss_service.SecuritySettingsServiceClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.create_security_settings(request)
+
+        return response
+
+    def update_security_settings(self, security_setting_id: str, **kwargs):
+        """Update specified CCAI Security Setting.
+
+        Args:
+            security_setting_id: Security Setting ID string in the following
+                format: projects/<PROJECT ID>/locations/<LOCATION ID>/
+                securitySettings/<SECURITY SETTINGS ID>
+
+        Returns:
+            response: a single Security Settings object of
+              types.SecuritySettings
+        """
+
+        security_settings = self.get_security_settings(security_setting_id)
+
+        # set intent attributes from kwargs
+        for key, value in kwargs.items():
+            setattr(security_settings, key, value)
+        paths = kwargs.keys()
+        mask = field_mask_pb2.FieldMask(paths=paths)
+
+        request = self.ss_types.UpdateSecuritySettingsRequest()
+
+        request.security_settings = security_settings
+        request.update_mask = mask
+
+        client_options = self._set_region(security_setting_id)
+        client = self.ss_service.SecuritySettingsServiceClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.update_security_settings(request)
+
+        return response
+
+    def delete_security_settings(self, security_setting_id: str):
+        """Delete the specified CCAI Security Setting.
+
+        Args:
           security_setting_id: Security Setting ID string in the following
-            format: projects/<PROJECT ID>/locations/<LOCATION ID>/
-            securitySettings/<SECURITY SETTINGS ID>
+                format: projects/<PROJECT ID>/locations/<LOCATION ID>/
+                securitySettings/<SECURITY SETTINGS ID>
+        """
+        request = self.ss_types.DeleteSecuritySettingsRequest()
+        request.name = security_setting_id
 
-      Returns:
-          response: a single Security Settings object of types.SecuritySettings
-      """
+        client_options = self._set_region(security_setting_id)
+        client = self.ss_service.SecuritySettingsServiceClient(
+            credentials=self.creds, client_options=client_options
+        )
 
-      security_settings = self.get_security_settings(security_setting_id)
-
-      # set intent attributes from kwargs
-      for key, value in kwargs.items():
-        setattr(security_settings, key, value)
-      paths = kwargs.keys()
-      mask = field_mask_pb2.FieldMask(paths=paths)
-
-      request = types.security_settings.UpdateSecuritySettingsRequest()
-
-      request.security_settings = security_settings
-      request.update_mask = mask
-
-      client_options = self._set_region(security_setting_id)
-      client = services.security_settings_service.SecuritySettingsServiceClient(
-          credentials=self.creds, client_options=client_options
-      )
-
-      response = client.update_security_settings(request)
-
-      return response
+        client.delete_security_settings(request)
