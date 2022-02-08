ee744d3fef9b0f1d53f6508dd1d91941731f8dc5
Patrick Marlow <kmaphoenix@gmail.com>
2022-02-08 14:53:41 -0600

    updating to v1.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='dfcx-scrapi',
-    version='1.1.1',
+    version='1.2.0',
     description='A high level scripting API for bot builders, developers, and\
       maintainers.',
     long_description=long_description,
