4963e4629353c7a0576c4f712dc5c4a1c47ebddd
Milad <63479762+MRyderOC@users.noreply.github.com>
2022-06-03 11:58:59 -0500

    TODO list added

--- src/dfcx_scrapi/builders/intent.py
+++ src/dfcx_scrapi/builders/intent.py
@@ -23,6 +23,12 @@ from google.cloud.dialogflowcx_v3beta1 import types
 
 class IntentBuilder:
     """Base Class for CX Intent builder."""
+    # Docs reference: dfcx - API
+    # parameter_checking() is an unused function
+    # update_intents function
+    # add_parameter: Check if the entity type exists
+    # Show info (basic, training phrases, parameters)
+
 
     def __init__(self):
         self.proto_obj = None
