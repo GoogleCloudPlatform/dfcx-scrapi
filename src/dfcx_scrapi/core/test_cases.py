3fbdad804578760f4888a34271b44e58689e928a
Patrick Marlow <kmaphoenix@gmail.com>
2022-10-24 09:58:34 -0500

    add batch create test cases and error handling

--- src/dfcx_scrapi/core/test_cases.py
+++ src/dfcx_scrapi/core/test_cases.py
@@ -16,6 +16,7 @@
 
 
 import logging
+import time
 from typing import Dict, List
 
 from google.cloud.dialogflowcx_v3beta1 import services
@@ -194,6 +195,33 @@ class TestCases(ScrapiBase):
         )
         response = client.create_test_case(request)
         return response
+
+    def batch_create_test_cases(
+      self,
+      test_cases_list: List[types.TestCase],
+      agent_id: str = None,
+      rate_limit: float = 0.5) -> Dict[str, Dict[str, types.TestCase]]:
+      """Create Test Cases in batch."""
+      failed_test_cases = {}
+
+      for test_case in test_cases_list:
+        try:
+          self.create_test_case(test_case, agent_id)
+
+        except Exception as create_error:
+          failed_test_cases[test_case.display_name] = {
+            'test_case': test_case,
+            'error': create_error}
+
+        time.sleep(rate_limit)
+
+      logging.info(f'{len(test_cases_list) - len(failed_test_cases)} Test' \
+        ' Cases successfully created.')
+      logging.info(f'{len(failed_test_cases)} Test Case failed to create.' \
+        ' Please see error log.')
+      
+      return failed_test_cases
+
 
     def get_test_case(self, test_case_id: str):
         """Get test case object from CX Agent.
