824007d863c6203a17fc9c2307989bf893da1df4
SeanScripts <oneil.p.sean@gmail.com>
2022-12-09 17:48:11 +0000

    Add function get_test_case_results

--- src/dfcx_scrapi/tools/agent_checker_util.py
+++ src/dfcx_scrapi/tools/agent_checker_util.py
@@ -49,6 +49,7 @@ class AgentCheckerUtil(ScrapiBase):
         self.webhooks = Webhooks(creds=self.creds, agent_id=self.agent_id)
         self.route_groups = TransitionRouteGroups(
             creds=self.creds, agent_id=self.agent_id)
+        self.test_cases = TestCases(creds=self.creds, agent_id=self.agent_id)
 
     def convert_intent(self, intent_id, agent_id, intents_map):
         intent_id_converted = str(agent_id) + '/intents/' + str(intent_id)
@@ -83,6 +84,74 @@ class AgentCheckerUtil(ScrapiBase):
         print('Flow not found')
         # TODO: Should throw error, but returning this probably will anyway
         return 'Invalid'
+    
+    def get_test_case_results(self, retest_all=False):
+        test_case_results = dfcx_tc.list_test_cases(self.agent_id)
+        retest = []
+        retest_names = []
+
+        display_names = []
+        ids = []
+        short_ids = []
+        tags = []
+        creation_times = []
+        flows = []
+        pages = []
+        test_results = []
+        test_times = []
+        passed = []
+
+        for response in test_case_results:
+            # Collect untested cases to be retested (or all if retest_all is True)
+            if retest_all or str(response.last_test_result.test_result) == 'TestResult.TEST_RESULT_UNSPECIFIED':
+                retest.append(response.name)
+                retest_names.append(response.display_name)
+                # Collect additional information for dataframe
+            display_names.append(response.display_name)
+            ids.append(response.name)
+            short_ids.append(response.name.split('/')[-1])
+            tags.append(','.join(response.tags))
+            creation_times.append(response.creation_time)
+            flows.append(convert_flow(response.test_config.flow, self.agent_id, self.flows_map))
+            pages.append(convert_page(response.test_config.page, response.test_config.flow, self.pages_map))
+            test_results.append(str(response.last_test_result.test_result))
+            test_times.append(response.last_test_result.test_time)
+            passed.append(str(response.last_test_result.test_result) == 'TestResult.PASSED')
+
+        # Create dataframe
+        test_case_df = pd.DataFrame({
+            'display_name': display_names, 
+            'id': ids, 
+            'short_id': short_ids, 
+            'tags': tags, 
+            'creation_time': creation_times, 
+            'start_flow': flows, 
+            'start_page': pages,
+            'test_result': test_results, 
+            'passed': passed, 
+            'test_time': test_times})
+
+        # Retest any that haven't been run yet
+        print('To retest:', len(retest))
+        if len(retest) > 0:
+            response = self.test_cases.batch_run_test_cases(retest, self.agent_id)
+            for result in response.results:
+                # Results may not be in the same order as they went in (oh well)
+                # Process the name a bit to remove the /results/id part at the end.
+                testCaseId_full = '/'.join(result.name.split('/')[:-2])
+                index = retest.index(testCaseId_full)
+                testCaseId = testCaseId_full.split('/')[-1]
+
+                # Update dataframe where id = testcaseId_full
+                #row = test_case_df.loc[test_case_df['id'] == testCaseId_full]
+                test_case_df.loc[test_case_df['id'] == testCaseId_full, 'short_id'] = testCaseId
+                test_case_df.loc[test_case_df['id'] == testCaseId_full, 'test_result'] = str(result.test_result)
+                test_case_df.loc[test_case_df['id'] == testCaseId_full, 'test_time'] = result.test_time
+                test_case_df.loc[test_case_df['id'] == testCaseId_full, 'passed'] = str(result.test_result) == 'TestResult.PASSED'
+
+        # This column is redundant, since we have passed (bool)
+        test_case_df = test_case_df.drop(columns=['test_result'])
+        return test_case_df
     
     """
     TODO: Methods to implement:
