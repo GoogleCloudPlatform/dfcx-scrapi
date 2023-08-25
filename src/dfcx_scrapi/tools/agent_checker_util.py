66f75780a50e800d5eb8e8f9aadd27943e434fcc
Patrick Marlow <kmaphoenix@gmail.com>
2023-08-25 15:28:22 -0500

    feat: implement recursion method for finding reachable pages in graph

--- src/dfcx_scrapi/tools/agent_checker_util.py
+++ src/dfcx_scrapi/tools/agent_checker_util.py
@@ -53,14 +53,8 @@ class AgentCheckerUtil(scrapi_base.ScrapiBase):
             creds=creds,
             scope=scope,
         )
-
-        STARTUP_TIME = time.time()
         self.agent_id = agent_id
-        self.extract = agents.Agents(agent_id)
-        PROCESSING_TIME = time.time()
-        logging.debug(f"STARTUP: {PROCESSING_TIME - STARTUP_TIME}")
-        self.data = self.extract.process_agent(agent_id, gcs_bucket_uri)
-        logging.debug(f"TOTAL PROCESSING: {time.time() - PROCESSING_TIME}")
+        self.active_intents_df = pd.DataFrame()
         self.special_pages = [
             "End Session",
             "End Flow",
@@ -68,80 +62,81 @@ class AgentCheckerUtil(scrapi_base.ScrapiBase):
             "Current Page",
             "Previous Page",
         ]
-        self.active_intents_df = pd.DataFrame()
 
-    # def find_reachable_pages(
-    #     self,
-    #     flow_name: str,
-    #     from_page: str = "Start",
-    #     intent_route_limit: Optional[int] = None,
-    #     include_groups: bool = True,
-    #     include_start_page_routes: bool = True,
-    #     include_meta: bool = False,
-    #     verbose: bool = False,
-    # ) -> List[str]:
-    #     """Finds all pages which are reachable by transition routes,
-    #     starting from a given page in a given flow. Either flow_id or
-    #     flow_name must be used.
+        STARTUP_TIME = time.time()
+        self.extract = agents.Agents(agent_id)
+        PROCESSING_TIME = time.time()
+        logging.debug(f"STARTUP: {PROCESSING_TIME - STARTUP_TIME}")
 
-    #     Args:
-    #       flow_name: The display name of the flow.
-    #       from_page: (Optional) The page to start from. If left blank, it will
-    #         start on the Start Page of the given flow.
-    #       intent_route_limit: (Optional) Default None. The maximum number of
-    #         intent routes to take. This can be used to answer questions like
-    #         "which pages can I reach within N turns, starting at this page?"
-    #       include_groups: (Optional) If true, intents from transition route
-    #         groups will be included, but only if they are actually referenced
-    #         on each given page in the traversal.
-    #       include_start_page_routes: (Optional) Default true. If true, intent
-    #         routes on the start page are always considered in scope. This is
-    #         how DFCX normally behaves.
-    #       include_meta: (Optional) Default False. If true, includes special
-    #         transition targets like End Session, End Flow, etc. as if they
-    #         are actual pages.
-    #       verbose: (Optional) If true, prints debug information about
-    #         route traversal.
+        self.data = self.extract.process_agent(agent_id, gcs_bucket_uri)
+        logging.debug(f"TOTAL PROCESSING: {time.time() - PROCESSING_TIME}")
 
-    #     Returns:
-    #       The list of reachable pages in this flow
-    #     """
-    #     flow_id = self._flows_map_rev.get(flow_name, None)
-    #     if not flow_id:
-    #         raise KeyError(f"Flow not found: {flow_name}")
+    def filter_special_pages(self, page: str, filter_special_pages: bool):
+        """Recursion helper to check for special page match."""
+        if filter_special_pages and page in self.special_pages:
+            return True
 
-    #     # Start at the start page...
-    #     reachable = [from_page]
-    #     conversation_path = [from_page]
-    #     # Technically this could be [0] or [1], or very rarely more than 1,
-    #     # depending on the routes that lead to current page...
-    #     min_intent_counts = [25]
-    #     presets = {}
-    #     page_data = self._get_page(
-    #         flow_name=flow_name,
-    #         page_name=from_page
-    #     )
-    #     params = {
-    #         "flow_id": flow_id,
-    #         "flow_name": flow_name,
-    #         "reachable": reachable,
-    #         "conversation_path": conversation_path,
-    #         "min_intent_counts": min_intent_counts,
-    #         "presets": presets,
-    #         "intent_route_limit": intent_route_limit,
-    #         "intent_route_count": 0,
-    #         "include_groups": include_groups,
-    #         "include_start_page_routes": include_start_page_routes,
-    #         "limit_intent_to_initial": False,
-    #         # This can't be stored here unless I want to add a lot of complex
-    #         # conditions to change it to False and back depending on the level
-    #         # of recursion
-    #         #"is_initial": True,
-    #         "include_meta": include_meta,
-    #         "verbose": verbose
-    #     }
-    #     self._find_reachable_pages_rec(page_data, params, is_initial=True)
-    #     return reachable
+        return False
+
+    def recurse_edges(self, edges: Dict[str, List[str]], page: str,
+                      visited: set, depth: int, max_depth: int,
+                      filter_special_pages: bool):
+        """Recursion method used to traverse the agent graph for page data.
+
+        Args:
+          edges: The set of graph edges collected from the agent.
+          page: The current Page Display Name
+          visited: A set of visited Page nodes
+          depth: The current recursion depth
+          max_depth: The max recursion depth
+          filter_special_pages: Will discard all self.special_pages from output
+            if set to False.
+        """
+        if depth == max_depth:
+            return visited
+
+        if page in edges:
+            for inner_page in edges[page]:
+                if self.filter_special_pages(inner_page, filter_special_pages):
+                    return visited
+
+                if inner_page not in visited:
+                    visited.add(inner_page)
+                    visited = self.recurse_edges(
+                        edges, inner_page, visited, depth+1, max_depth,
+                        filter_special_pages)
+
+        return visited
+
+    def get_reachable_pages(
+            self,
+            flow_display_name: str,
+            page_display_name: str = "Start Page",
+            max_depth: int = 1,
+            filter_special_pages = True) -> List[str]:
+        """Get all pages in the graph that are reachable via transition routes,
+        starting from a given Flow and Page.
+
+        Args:
+          flow_display_name: The display name of the flow
+          page_display_name: The display name of the page. Defaults to
+            "Start Page"
+          max_depth: The max recursion depth to search the graph from the
+            provided starting point. For example, a max_depth of 2 would produce
+            all reachable Pages that are 2 transition routes away from the
+            starting Flow/Page. Defaults to 1.
+          filter_special_pages: Will filter out all self.special_pages. Defaults
+            to True.
+          """
+        if page_display_name in ["START", "START_PAGE", "Start", "Start Page"]:
+            page_display_name = "Start Page"
+            page_display_name = f"{flow_display_name}: {page_display_name}"
+
+        visited = self.recurse_edges(
+            self.data.graph.edges, page_display_name, set(), 0, max_depth,
+            filter_special_pages)
+
+        return list(visited)
 
     def active_intents_to_dataframe(self) -> pd.DataFrame:
         """Gets all intents referenced in the agent, across all flows,
