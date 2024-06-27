2c3888910294323ff871057b3a494c67d3e9da8e
Patrick Marlow <kmaphoenix@gmail.com>
2024-06-27 11:39:23 -0500

    feat: adding new agent builder classes

---
+++ src/dfcx_scrapi/core/examples.py
@@ -0,0 +1,235 @@
+"""Examples class for Generative Agents."""
+
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dfcx_scrapi.core import scrapi_base
+from typing import Dict, List, Union
+
+from google.cloud.dialogflowcx_v3beta1 import services
+from google.cloud.dialogflowcx_v3beta1 import types
+from google.protobuf import field_mask_pb2
+
+
+class Examples(scrapi_base.ScrapiBase):
+    """Core Class for CX Examples Resource functions."""
+
+    def __init__(
+        self,
+        creds_path: str = None,
+        creds_dict: Dict = None,
+        creds=None,
+        scope=False,
+        example_id: str = None,
+        playbook_id: str = None,
+        agent_id: str = None,
+    ):
+        super().__init__(
+            creds_path=creds_path,
+            creds_dict=creds_dict,
+            creds=creds,
+            scope=scope,
+        )
+
+        if example_id:
+            self.example_id = example_id
+            self.client_options = self._set_region(example_id)
+
+        if playbook_id:
+            self.playbook_id = playbook_id
+
+        if agent_id:
+            self.agent_id = agent_id
+
+    def get_examples_map(self, playbook_id: str = None, reverse=False):
+        """Exports Agent Example Names and UUIDs into a user friendly dict.
+
+        Args:
+          playbook_id: the formatted CX Agent Playbook ID to use
+          reverse: (Optional) Boolean flag to swap key:value -> value:key
+
+        Returns:
+          Dictionary containing Example UUIDs as keys and display names as values
+        """
+        if not playbook_id:
+            playbook_id = self.playbook_id
+
+        if reverse:
+            examples_dict = {
+                example.display_name: example.name
+                for example in self.list_examples(playbook_id)
+            }
+
+        else:
+            examples_dict = {
+                example.name: example.display_name
+                for example in self.list_examples(playbook_id)
+            }
+
+        return examples_dict
+
+    @scrapi_base.api_call_counter_decorator
+    def list_examples(
+        self,
+        playbook_id: str = None,
+        language_code: str = "en") -> List[types.Example]:
+        """Get a List of all Examples in the specified Playbook.
+
+        Args:
+          playbook_id: the properly formatted Playbook ID string
+          language_code: Specifies the language of the Examples listed. While the
+            majority of contents of a Page is language agnostic, the contents
+            in the "Agent Says" and similar parts of a Page are affected by
+            language code.
+
+        Returns:
+          A List of CX Example objects for the specific Playbook ID
+        """
+        if not playbook_id:
+            playbook_id = self.playbook_id
+
+        request = types.example.ListExamplesRequest()
+        request.parent = playbook_id
+        request.language_code = language_code
+
+        client_options = self._set_region(playbook_id)
+        client = services.examples.ExamplesClient(
+            credentials=self.creds, client_options=client_options
+        )
+        response = client.list_examples(request)
+
+        cx_examples = []
+        for page in response.pages:
+            for cx_example in page.examples:
+                cx_examples.append(cx_example)
+
+        return cx_examples
+
+    @scrapi_base.api_call_counter_decorator
+    def get_example(self, example_id: str = None) -> types.Example:
+        """Get a single CX Example object based on the provided Example ID.
+
+        Args:
+          example_id: a properly formatted CX Example ID
+
+        Returns:
+          A single CX Example Object
+        """
+        if not example_id:
+            example_id = self.example_id
+
+        client_options = self._set_region(example_id)
+        client = services.examples.ExamplesClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.get_example(name=example_id)
+
+        return response
+
+    @scrapi_base.api_call_counter_decorator
+    def create_example(
+        self,
+        playbook_id: str = None,
+        obj: types.Example = None,
+        **kwargs
+    ) -> types.Example:
+        """Create a single CX Example object in the specified Playbook ID.
+
+        Args:
+          playbook_id: the CX Playbook ID where the Example object will be created
+          obj: (Optional) a CX Example object of types.Example
+
+        Returns:
+          A copy of the successful Example object that was created
+        """
+        if not playbook_id:
+            playbook_id = self.playbook_id
+
+        if obj:
+            example = obj
+            example.name = ""
+        else:
+            example = types.example.Example()
+
+        for key, value in kwargs.items():
+            setattr(example, key, value)
+
+        client_options = self._set_region(playbook_id)
+        client = services.examples.ExamplesClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.create_example(parent=playbook_id, example=example)
+
+        return response
+
+    @scrapi_base.api_call_counter_decorator
+    def update_example(
+        self,
+        example_id: str = None,
+        obj: types.Example = None,
+        **kwargs
+    ) -> types.Example:
+        """Update a single CX Example object.
+
+        Args:
+          example_id: the CX Example ID to update
+          obj: (Optional) a CX Example object of types.Example
+
+        Returns:
+          A copy of the successful Example object that was created
+        """
+        if obj:
+            example = obj
+            example.name = example_id
+        else:
+            if not example_id:
+                example_id = self.example_id
+            example = self.get_example(example_id)
+
+        for key, value in kwargs.items():
+            setattr(example, key, value)
+        paths = kwargs.keys()
+        mask = field_mask_pb2.FieldMask(paths=paths)
+
+        client_options = self._set_region(example_id)
+        client = services.examples.ExamplesClient(
+            credentials=self.creds, client_options=client_options
+        )
+
+        response = client.update_example(example=example, update_mask=mask)
+
+        return response
+
+    @scrapi_base.api_call_counter_decorator
+    def delete_example(
+        self, example_id: str = None,
+        obj: types.Example = None):
+        """Deletes the specified Example.
+
+        Args:
+          example_id: The formatted CX Example ID to delete.
+          obj: (Optional) a CX Example object of types.Example
+        """
+        if not example_id:
+            example_id = self.example_id
+
+        if obj:
+            example_id = obj.name
+
+        client_options = self._set_region(example_id)
+        client = services.examples.ExamplesClient(
+            credentials=self.creds, client_options=client_options)
+        client.delete_example(name=example_id)
