80c18e996b74de6407102435938ceca50d7834ab
Patrick Marlow <kmaphoenix@gmail.com>
2024-07-25 09:28:04 -0500

    chore: bump to version 1.11.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='dfcx-scrapi',
-    version='1.11.0',
+    version='1.11.1',
     description='A high level scripting API for bot builders, developers, and\
       maintainers.',
     long_description=long_description,
