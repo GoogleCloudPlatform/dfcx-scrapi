227e0b245ba5008d39277af59e4916036afccc8f
Carina Li <carinali@google.com>
2025-02-27 00:56:25 -0800

    Fix lint I001 import sorting error

--- src/dfcx_scrapi/tools/metrics.py
+++ src/dfcx_scrapi/tools/metrics.py
@@ -29,12 +29,19 @@ import pandas as pd
 from google import genai
 from rouge_score import rouge_scorer
 from tqdm.contrib import concurrent
-from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
-from dfcx_scrapi.core.scrapi_base import (EMBEDDING_MODELS_NO_DIMENSIONALITY,
-                                          get_generate_content_config,
-                                          handle_api_error, ratelimit,
-                                          retry_api_call)
+from vertexai.language_models import (
+    TextEmbeddingInput,
+    TextEmbeddingModel
+)
+
+from dfcx_scrapi.core.scrapi_base import (
+    EMBEDDING_MODELS_NO_DIMENSIONALITY,
+    get_generate_content_config,
+    handle_api_error,
+    ratelimit,
+    retry_api_call
+)
 
 # logging config
 logging.basicConfig(
